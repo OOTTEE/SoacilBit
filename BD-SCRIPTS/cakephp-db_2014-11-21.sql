dfbfaa20a82fba864a855a251e0090b8e0415c4d
Javier Lorenzo Martin <javier.ote.ote@gmail.com>
2014-11-25 13:19:43 +0100

    SCRIPT BD ACTUALIZADO
    Se ha añadido el campo id, y el campo solicitud en la tabla friends,
    tambien se han declarado como unicas la convinacion de los campo
    user_id_user y user_id_friend

--- BD-SCRIPTS/cakephp-db_2014-11-21.sql
+++ BD-SCRIPTS/cakephp-db_2014-11-21.sql
@@ -3,10 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 21, 2014 at 05:41 PM
+-- Generation Time: Nov 25, 2014 at 12:17 PM
 -- Server version: 5.5.40
 -- PHP Version: 5.4.34
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -19,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `cakephp`
 --
-CREATE DATABASE IF NOT EXISTS `cakephp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `cakephp`;
 
 -- --------------------------------------------------------
 
@@ -30,7 +29,7 @@ USE `cakephp`;
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `contenido` varchar(300) NOT NULL,
@@ -45,10 +44,23 @@ CREATE TABLE `comments` (
 
 DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
+  `id` int(11) NOT NULL,
   `user_id_user` int(11) NOT NULL,
+  `fecha` date NOT NULL,
   `user_id_friend` int(11) NOT NULL,
-  `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `solicitud` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`id`, `user_id_user`, `fecha`, `user_id_friend`, `solicitud`) VALUES
+(1, 21, '2014-11-24', 22, 1),
+(2, 21, '2014-11-24', 23, 1),
+(3, 24, '2014-11-18', 22, 1),
+(6, 22, '2014-11-25', 23, 1),
+(7, 22, '2014-11-25', 22, 1);
 
 -- --------------------------------------------------------
 
@@ -70,11 +82,24 @@ CREATE TABLE `likes` (
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `post` text NOT NULL,
   `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `post`, `fecha`) VALUES
+(7, 22, 'pene\r\n', '2014-11-24'),
+(8, 22, 'asdfasfasdfasdf', '2014-11-24'),
+(9, 22, 'asdfasfasdfasdf', '2014-11-24'),
+(10, 21, 'user 21', '2014-11-24'),
+(11, 23, 'user 23 ', '2014-11-04'),
+(12, 24, 'hector\r\n', '2014-11-24'),
+(13, 22, 'siiii tiiiio\r\n', '2014-11-24');
 
 -- --------------------------------------------------------
 
@@ -96,11 +121,21 @@ CREATE TABLE `requests` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(16) NOT NULL,
   `nombre` varchar(80) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `nombre`, `password`) VALUES
+(21, 'OTE', 'OTE', '$2a$10$b105x9CpBjLCI0Eptd1eh.sfr3zNJksfNZ7uMsIYXjMq/gG1JgDqG'),
+(22, 'admin', 'admin', '$2a$10$VJxU/996yt3p5jQEhPIaReBJKr6cmGqA2SbkfHltdzbqtPeA5KjZm'),
+(23, 'OTE1', 'adrian', '$2a$10$mJox3RsFpTwpNxPb4gqnsuT7sENuBQ4tMVlU07Vy6vYEPOqZJjnb.'),
+(24, 'hector', 'hector', '$2a$10$hKQ4RfIprCYXBx/g6htlzeBEb2nwO4KobSKLvK0rXKsKKfkr8DV/a');
 
 --
 -- Indexes for dumped tables
@@ -110,37 +145,37 @@ CREATE TABLE `users` (
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `friends`
 --
 ALTER TABLE `friends`
- ADD PRIMARY KEY (`user_id_user`);
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `user_id_user` (`user_id_user`,`user_id_friend`);
 
 --
 -- Indexes for table `likes`
 --
 ALTER TABLE `likes`
- ADD PRIMARY KEY (`post_id`);
+ADD PRIMARY KEY (`post_id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
- ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `requests`
 --
 ALTER TABLE `requests`
- ADD PRIMARY KEY (`user_id_user`);
+ADD PRIMARY KEY (`user_id_user`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `alias` (`username`);
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `alias` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -152,15 +187,21 @@ ALTER TABLE `users`
 ALTER TABLE `comments`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;SET FOREIGN_KEY_CHECKS=1;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
